c8fb3a3450dc8770e496b0aa3af3578b78cc54ae
anon-kae <anon.ksl42@gmail.com>
2024-03-31 01:24:47 +0700

    feat: init swagger doc of get wallet by wallet id

--- wallet/wallet_test.go
+++ wallet/wallet_test.go
@@ -33,7 +33,22 @@ func (m *mockStorerSuccess) Wallets(walletType string) ([]Wallet, error) {
 }
 
 func (m *mockStorerSuccess) Wallet(id uint64) (Wallet, error) {
-	return Wallet{}, nil
+	mockDateString := "2024-02-03T00:00:00Z"
+	layout := "2006-01-02T15:04:05Z"
+	createdAt, err := time.Parse(layout, mockDateString)
+	if err != nil {
+		panic(err)
+	}
+
+	return Wallet{
+		ID:         1,
+		UserID:     1,
+		UserName:   "John Doe",
+		WalletName: "John's Wallet",
+		WalletType: "Create Card",
+		CreatedAt:  createdAt,
+		Balance:    100,
+	}, nil
 }
 
 type mockStorerFailure struct{}
@@ -43,7 +58,7 @@ func (m *mockStorerFailure) Wallets(walletType string) ([]Wallet, error) {
 }
 
 func (m *mockStorerFailure) Wallet(id uint64) (Wallet, error) {
-	return Wallet{}, nil
+	return Wallet{}, errors.New("error fetching wallet")
 }
 
 func TestWallet(t *testing.T) {
@@ -79,6 +94,24 @@ func TestWallet(t *testing.T) {
 			assert.Equal(t, http.StatusOK, rec.Code)
 
 			expectedResponse := `[{"id":1,"balance":100,"created_at":"2024-02-03T00:00:00Z","user_id":1,"user_name":"John Doe","wallet_name":"John's Wallet","wallet_type":"Create Card"}]`
+			assert.JSONEq(t, expectedResponse, rec.Body.String())
+		}
+	})
+
+	t.Run("Get wallet by walletId: not found", func(t *testing.T) {
+		// Setup
+		e := echo.New()
+		req := httptest.NewRequest(http.MethodGet, "/api/v1/wallet/122", nil)
+		rec := httptest.NewRecorder()
+		c := e.NewContext(req, rec)
+
+		h := New(&mockStorerFailure{})
+
+		// Assertions
+		if assert.NoError(t, h.WalletHandlerByID(c)) {
+			assert.Equal(t, http.StatusBadRequest, rec.Code)
+
+			expectedResponse := `{"message":"Invalid wallet id"}`
 			assert.JSONEq(t, expectedResponse, rec.Body.String())
 		}
 	})
