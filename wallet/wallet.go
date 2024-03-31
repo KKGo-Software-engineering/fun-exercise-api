bcf95a7b963ef4324e8e34b41f94d170733310e5
anon-kae <anon.ksl42@gmail.com>
2024-03-31 19:56:16 +0700

    feat: add test for update wallet

--- wallet/wallet.go
+++ wallet/wallet.go
@@ -7,7 +7,7 @@ type Wallet struct {
 	UserID     int       `json:"user_id" example:"1"`
 	UserName   string    `json:"user_name" example:"John Doe"`
 	WalletName string    `json:"wallet_name" example:"John's Wallet"`
-	WalletType string    `json:"wallet_type" example:"Create Card"`
+	WalletType string    `json:"wallet_type" example:"Credit Card"`
 	Balance    float64   `json:"balance" example:"100.00"`
 	CreatedAt  time.Time `json:"created_at" example:"2024-03-25T14:19:00.729237Z"`
 }
